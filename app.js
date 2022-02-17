584a1c706a611eaf111d1b5192de80e9979818d7
khanshorif331 <khanshorif331@gmail.com>
2022-02-17 19:52:55 +0600

    first event handler complete

--- app.js
+++ app.js
@@ -12,17 +12,27 @@
 
 
 document.getElementById('calculate-btn').addEventListener('click',function(){
+    // taking all the input values
     const income=document.getElementById('income-input');
     const incomeTotal = income.value;
 
     const foodInput=document.getElementById('food-input');
-    const foodTotal = foodInput.value;
+    const foodTotal = parseFloat(foodInput.value) ;
 
     const rentInput=document.getElementById('rent-input');
-    const rentTotal = rentInput.value;
+    const rentTotal = parseFloat(rentInput.value);
 
     const clothesInput=document.getElementById('clothes-input');
-    const clothesTotal = clothesInput.value;
+    const clothesTotal = parseFloat(clothesInput.value);
 
-    console.log(incomeTotal,foodTotal,rentTotal,clothesTotal);
+    const sum = foodTotal+rentTotal+clothesTotal;
+
+    const totalExpense = document.getElementById('total-expenses');
+    totalExpense.innerText= sum;
+
+    const balance = document.getElementById('balance');
+    const currentBalance= incomeTotal-sum;
+    balance.innerText = currentBalance;
+
+    // console.log(totalExpense.innerText);
 })
